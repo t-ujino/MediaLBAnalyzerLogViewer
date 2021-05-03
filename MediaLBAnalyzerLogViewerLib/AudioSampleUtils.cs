084e2384ed5e3633d61440cde3f452e84508be9c
Tadashi Ujino <t.ujino.priv+github@gmail.com>
2021-05-03 23:30:51 +0900

    :zap: byte列を16/24/32bitのオーディオサンプル値として変換する関数を追加

---
+++ MediaLBAnalyzerLogViewerLib/AudioSampleUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaLBAnalyzerLogViewerLib
+{
+    public enum Endian
+    {
+        LittleEndian,
+        BigEndian
+    }
+
+    public class AudioSampleUtils
+    {
+        static public double Convert16bitToDouble(byte[] data, Endian endian = Endian.LittleEndian)
+        {
+            var array = (endian == Endian.LittleEndian) ? data : data.Reverse().ToArray();
+            var value = BitConverter.ToInt16(array);
+            return value / Math.Abs((double)Int16.MinValue);
+        }
+
+        static public double Convert32bitToDouble(byte[] data, Endian endian = Endian.LittleEndian)
+        {
+            var array = (endian == Endian.LittleEndian) ? data : data.Reverse().ToArray();
+            var value = BitConverter.ToInt32(array);
+            return value / Math.Abs((double)Int32.MinValue);
+        }
+
+        static public double Convert24bitToDouble(byte[] data, Endian endian = Endian.LittleEndian)
+        {
+            var array = (endian == Endian.LittleEndian) ?
+                new byte[] { 0x00 }.Concat(data) : new byte[] { 0x00 }.Concat(data.Reverse());
+            return Convert32bitToDouble(array.ToArray());
+        }
+    }
+}
---
+++ MediaLBAnalyzerLogViewerLibTests/AudioSampleUtilsTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MediaLBAnalyzerLogViewerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaLBAnalyzerLogViewerLib.Tests
+{
+    [TestClass()]
+    public class AudioSampleUtilsTests
+    {
+        [TestMethod()]
+        public void Convert16bitToDoubleTest()
+        {
+            var value = new byte[] { 0x01, 0x02 };
+            var result = AudioSampleUtils.Convert16bitToDouble(value, Endian.BigEndian);
+            var expect = 0x0102 / Math.Abs((double)Int16.MinValue);
+            Assert.AreEqual(true, Math.Abs(expect - result) < 1e-5);
+
+            result = AudioSampleUtils.Convert16bitToDouble(value, Endian.LittleEndian);
+            expect = 0x0201 / Math.Abs((double)Int16.MinValue);
+            Assert.AreEqual(true, Math.Abs(expect - result) < 1e-5);
+        }
+
+        [TestMethod()]
+        public void Convert32bitToDoubleTest()
+        {
+            var value = new byte[] { 0x01, 0x02, 0x03, 0x04 };
+            var result = AudioSampleUtils.Convert32bitToDouble(value, Endian.BigEndian);
+            var expect = 0x01020304 / Math.Abs((double)Int32.MinValue);
+            Assert.AreEqual(true, Math.Abs(expect - result) < 1e-5);
+
+            result = AudioSampleUtils.Convert32bitToDouble(value, Endian.LittleEndian);
+            expect = 0x04030201 / Math.Abs((double)Int32.MinValue);
+            Assert.AreEqual(true, Math.Abs(expect - result) < 1e-5);
+        }
+
+        [TestMethod()]
+        public void Convert24bitToDoubleTest()
+        {
+            var value = new byte[] { 0x01, 0x02, 0x03 };
+            var result = AudioSampleUtils.Convert24bitToDouble(value, Endian.BigEndian);
+            var expect = 0x01020300 / Math.Abs((double)Int32.MinValue);
+            Assert.AreEqual(true, Math.Abs(expect - result) < 1e-5);
+
+            result = AudioSampleUtils.Convert24bitToDouble(value, Endian.LittleEndian);
+            expect = 0x03020100 / Math.Abs((double)Int32.MinValue);
+            Assert.AreEqual(true, Math.Abs(expect - result) < 1e-5);
+        }
+    }
+}
